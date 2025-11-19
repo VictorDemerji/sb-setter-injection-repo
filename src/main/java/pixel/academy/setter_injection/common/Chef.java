6fd6e8d6cd9c9dc945646382607ed333c44527eb
VictorDemerji <demerji.v1@gmail.com>
2025-11-19 20:01:48 +0200

    Initial commit

---
+++ src/main/java/pixel/academy/setter_injection/common/Chef.java
@@ -0,0 +1,7 @@
+package pixel.academy.setter_injection.common;
+
+public interface Chef {
+
+    String getDailyRecipe();
+
+}
---
+++ src/main/java/pixel/academy/setter_injection/common/ItalianChef.java
@@ -0,0 +1,11 @@
+package pixel.academy.setter_injection.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItalianChef implements Chef {
+    @Override
+    public String getDailyRecipe() {
+        return "Prepare pasta al pomodoro with fresh tomatoes and basil!";
+    }
+}
